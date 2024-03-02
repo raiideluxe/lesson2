ddfd22665763954fcb8ae98c5208bf51958c2d15
raiideluxe <artemreshetnikov.one@gmail.com>
2024-03-02 13:05:38 +0400

    commit 20324

--- task10.py
+++ task10.py
@@ -1,1 +1,16 @@
-print('hello')
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.linspace(-10 ,10, 1000)
+y = np.linspace(-10 ,10, 1000)
+plt.figure(figsize=(10,5))
+plt.grid(lw=0.5, ls='--')
+plt.plot(x, ((np.sin(x**2+y**2))/(x**2+y**2)),lw = 4, color = 'red',zorder = 0)
+plt.show()
+
+
+
+
+
+
+
