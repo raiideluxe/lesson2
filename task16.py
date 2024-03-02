46b685a139a3f27f24fdba13dab50a362b13acf7
raiideluxe <artemreshetnikov.one@gmail.com>
2024-03-02 13:50:35 +0400

    commit 20324

--- task16.py
+++ task16.py
@@ -1,1 +1,17 @@
-print('hello')
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import warnings
+from scipy.optimize import minimize
+
+mpl.style.use('seaborn-white')
+warnings.filterwarnings('ignore')
+dx = 0.001
+x = np.arange(-10, 10, dx)
+f0 = lambda x: (x - 0.5)**2
+plt.plot(x, f0(x))
+plt.show()
+
+result = minimize(f0, x0=1)
+print(result)
+
