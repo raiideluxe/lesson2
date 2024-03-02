d9d9a9953eaa9a9994920c11c098df0bd9d427c4
raiideluxe <artemreshetnikov.one@gmail.com>
2024-03-02 14:33:09 +0400

    commit 20324

--- task19.py
+++ task19.py
@@ -1,1 +1,13 @@
-print('hello')
+from scipy.optimize import minimize
+import numpy as np
+import math
+
+x0=float(input())
+def myfunc(x):
+    return x**2*(x-4)**2*math.exp(-x**2)
+
+result = minimize(lambda x: -myfunc(x), x0, method='Nelder-Mead')
+
+xmax = round(result.x[0],2)
+print({xmax})
+
