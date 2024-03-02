6dd99f22d1661394582922e875be1b44f08d9d7a
raiideluxe <artemreshetnikov.one@gmail.com>
2024-03-02 13:44:44 +0400

    commit 20324

--- task14.py
+++ task14.py
@@ -1,1 +1,15 @@
-print('hello')
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.integrate import odeint
+
+def dfdt(f,t):
+    return f + t
+
+def dfdt_vector(f,t):
+    return np.array([dfdt(f[0],t)])
+
+f0=float(input())
+t_values = np.linspace(0,1,100)
+f_values = odeint(dfdt_vector, f0, t_values)
+
+print(f"{f_values[-1][0]:.2f}")
