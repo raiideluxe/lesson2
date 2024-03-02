bec341d0f01f5f8404fa9b272aad11b1ae36d486
raiideluxe <artemreshetnikov.one@gmail.com>
2024-03-02 13:52:22 +0400

    commit 20324

--- task16.py
+++ task16.py
@@ -4,14 +4,23 @@ import matplotlib as mpl
 import warnings
 from scipy.optimize import minimize
 
+def get_path(xc):
+ global path
+ path.append(xc)
+
 mpl.style.use('seaborn-white')
 warnings.filterwarnings('ignore')
 dx = 0.001
 x = np.arange(-10, 10, dx)
-f0 = lambda x: (x - 0.5)**2
-plt.plot(x, f0(x))
+f = lambda x: x**2 * ( 1 - 0.1 * (x) **2 )* np.exp(- 0.1 * (x)**2)
+fig, ax = plt.subplots()
+ax.set(xlabel='x', ylabel='f(x)')
+ax.plot(x, f(x))
 plt.show()
 
-result = minimize(f0, x0=1)
+x0 = 2.4
+path = [x0]
+result = minimize(f, x0=x0, tol=1e-2, callback=get_path)
+x1 = result.x
 print(result)
 
