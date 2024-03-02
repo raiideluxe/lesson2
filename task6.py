5c2f5be37e0f21972e5d997a7aaf0574b5dd8399
raiideluxe <artemreshetnikov.one@gmail.com>
2024-03-02 12:28:02 +0400

    commit 20324

--- task6.py
+++ task6.py
@@ -1,1 +1,11 @@
-print('hello')
+import numpy as np
+A=np.zeros((100,100))
+def nonzero(A):
+    nonzero_coordinates = np.nonzero(A)
+    x = nonzero_coordinates[0][0]
+    y = nonzero_coordinates[1][0]
+    return x,y
+
+A[56][70]=1
+
+print(nonzero(A))
--- task7.py
+++ task7.py
@@ -1,1 +1,3 @@
-print('hello')
+import numpy as np
+massive=[3,2,5,4,1,7]
+
