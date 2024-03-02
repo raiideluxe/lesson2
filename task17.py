86fe378fa430bb9f731a16a5e940dacc7a9b786a
raiideluxe <artemreshetnikov.one@gmail.com>
2024-03-02 14:08:32 +0400

    commit 20324

--- task17.py
+++ task17.py
@@ -4,27 +4,35 @@ import matplotlib as mpl
 import warnings
 from scipy.optimize import minimize
 
+def get_path(xc):
+    global path
+    path.append(xc)
+
 def gauss(z, sigma, x0, y0):
- x, y = z
- return np.exp(-((x-x0)**2 + (y-y0)**2) / sigma**2)
+    x, y = z
+    return np.exp(-((x-x0)**2 + (y-y0)**2) / sigma**2)
+
 neg_gauss = lambda z, sigma, x0, y0: -gauss(z, sigma, x0, y0)
+
 x = np.linspace(-20, 20, 100)
 y = np.linspace(-20, 20, 100)
 X, Y = np.meshgrid(x, y)
 Z = gauss((X, Y), 100, 0, 0)
 fig, ax = plt.subplots(1, 1, figsize=(6, 6))
-contours = plt.contour(X, Y, Z, 15, colors="black", linewidths=2,
-linestyles='-.')
+contours = plt.contour(X, Y, Z, 15, colors="black", linewidths=2, linestyles='-.')
 ax.clabel(contours, inline = True, fontsize=16)
 contours = plt.contourf(X, Y, Z, 200, cmap=plt.cm.hsv, alpha=0.7)
 ax.set(xlabel="Ox", ylabel="Oy")
 plt.show()
 
 from scipy.optimize import minimize
+
 a, b = 0, 0
+
 def func(t, a, b):
- x, y = t[0], t[1]
- return (x + y)**2 - 2 * x * (y + a) - 2 * y * b + a + b
+    x, y = t[0], t[1]
+    return (x + y)**2 - 2 * x * (y + a) - 2 * y * b + a + b
+
 res = minimize(func, ((0, 0), ), args=(a, b))
 print(" ".join(str(i) for i in res.x))
 
--- task18.py
+++ task18.py
@@ -1,1 +1,36 @@
-print('hello')
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import warnings
+from scipy.optimize import minimize
+
+def mixed(z, *args):
+    return np.sum(neg_gauss(z, *params) for params in args)
+Z = mixed((X, Y), (10, -5, -12), (7, 5, 5), (9, -5, 10))
+fig, ax = plt.subplots(1, 1, figsize=(8, 8))
+contours = ax.contour(X, Y, Z, 16, colors="black", linewidths=2,
+linestyles='-.')
+ax.clabel(contours, inline=True, fontsize=16)
+contours = ax.contourf(X, Y, Z, 200, cmap=plt.cm.jet)
+plt.show()
+
+# Gradient method
+x0 = (-10, -2)
+path = [x0]
+result = minimize(mixed, x0,
+ args=((10, -5, -12), (7, 5, 5), (9, -5, 10)),
+ callback=get_path,
+ )
+Z = mixed((X, Y), (10, -5, -12), (7, 5, 5), (9, -5, 10))
+path = np.array(path)
+fig, ax = plt.subplots(1, 2, figsize=(20, 10))
+contours = ax[0].contour(X, Y, Z, 16, colors="black", linewidths=2,
+linestyles='-.')
+ax[0].clabel(contours, inline=True, fontsize=16)
+contours = ax[0].contourf(X, Y, Z, 200, cmap=plt.cm.jet)
+ax[0].scatter(path[:, 0], path[:, 1], s=1600, c='yellow',
+ marker='*',
+ alpha=1,
+ edgecolor='black',
+ linewidth=2,
+ zorder=1
