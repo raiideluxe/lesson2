84e2cc097f564c47522715eb909f65099f22104f
raiideluxe <artemreshetnikov.one@gmail.com>
2024-03-02 14:41:31 +0400

    commit 20324

--- task20.py
+++ task20.py
@@ -1,1 +1,13 @@
-print('hello')
+from scipy.optimize import minimize
+import numpy as np
+import math
+#a=float(input())
+#b=float(input())
+x0y0 = [0,0]
+def myfunc(vars,a,b):
+    x,y = vars
+    return (x+y)**2-2*x*(y+a)-2*y*b+a+b
+a=8
+b=6
+result = minimize(myfunc, x0y0, args=(a,b))
+print(result)
