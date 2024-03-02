a8219e60b6fa0bcf828a0a9771405397798c29fa
raiideluxe <artemreshetnikov.one@gmail.com>
2024-03-02 13:24:56 +0400

    commit 20324

--- task12.py
+++ task12.py
@@ -1,1 +1,13 @@
-print('hello')
+from scipy.integrate import odeint
+import matplotlib.pyplot as plt
+import numpy as np
+
+func = lambda y, t: t**2
+dt=1e-3
+t = np.arange(0,1,dt)
+res = odeint(func, y0 = 0, t = t)
+plt.figure(figsize=(5,4))
+plt.plot(t, res)
+plt.plot(t[::50], t[::50]**3/3, 'o')
+plt.show()
+
