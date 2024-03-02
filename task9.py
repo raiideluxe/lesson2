abd627a629fc3431492b26d96afd86884df14f16
raiideluxe <artemreshetnikov.one@gmail.com>
2024-03-02 13:00:26 +0400

    commit 20324

--- task9.py
+++ task9.py
@@ -1,1 +1,9 @@
-print('hello')
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.linspace(-10 ,10, 1000)
+plt.figure(figsize=(8,3))
+plt.grid(lw=0.5, ls='--')
+plt.plot(x, ((np.sin(2*x))**2)*np.exp((x+2)**2/(10**2)),lw = 4, color = 'red',zorder = 0)
+plt.show()
